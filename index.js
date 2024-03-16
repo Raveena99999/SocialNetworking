a1653d95d255d1a92ad9be66da131d738a9af9fb
Raveena Banduni <raveenabanduni06@gmail.com>
2024-03-16 22:45:51 +0530

    link change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: "http://127.0.0.1:5173",
+        origin: "https://social-zeta-brown.vercel.app",
         credentials: true,
         httpOnly: true,
     })
